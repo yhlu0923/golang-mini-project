93dbb63133cdc48889dd224538928e345cfc0011
alan8585 <alanlin9006@gmail.com>
2022-01-06 02:26:31 +0800

    Update draw

--- vendor/local-packages/draw-picture/draw_picture.go
+++ vendor/local-packages/draw-picture/draw_picture.go
@@ -1,45 +1,43 @@
-// Function of parsing picture
-
-package draw_picture
+package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
+	"io/ioutil"
 	"regexp"
 	"strings"
 	"time"
 	"math/rand"
 )
 
-func Get_html(url string) string {
-	fmt.Println("Fetch Url", url)
+func get_html (url string) string {
 	client := &http.Client{}
 	request, _ := http.NewRequest("GET", url, nil)
 	request.Header.Set("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
 	response, err := client.Do(request)
 	if err != nil {
 		fmt.Println("Http get err:", err)
-		return ""
+        return ""
 	}
 	if response.StatusCode != 200 {
 		fmt.Println("Http status:", response.StatusCode)
 		return ""
 	}
 	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("ReadAll get err:", err)
-		return ""
-	}
 	response.Body.Close()
 	return string(body)
 }
 
 func Parse(body string) string {
 	body = strings.Replace(body, "\n", "", -1)
-	img_reg := regexp.MustCompile(`<img class=(.*?)>`)
-	src_reg := regexp.MustCompile(`src="(.*?)"`)
-	img_url := src_reg.FindAllStringSubmatch(img_reg.FindAllStringSubmatch(body, -1)[Rnd(5) + 1][1], -1)[0][1]
+	img_reg := regexp.MustCompile(`<a class="img"    href='(.*?)'aria`)
+	select_idx := img_reg.FindAllStringSubmatch(body, -1)[Rnd(10)][1]
+	
+	body2 := get_html("https://tw.images.search.yahoo.com/" + select_idx)
+	body2 = strings.Replace(body2, "\n", "", -1)
+	
+	src_reg := regexp.MustCompile(`<img src=('|")(.*?)&.*?('|")(.*?)>`)
+	img_url := src_reg.FindAllStringSubmatch(body2, -1)[0][2]
+	
 	return img_url
 }
 
